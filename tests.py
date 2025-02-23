ce6fa8b8134eac9add45a0a0092d64659df6dfd7
Kai Soman <kaisoman56@gmail.com>
2025-02-23 19:47:29 +1100

    unit conversion test

--- tests.py
+++ tests.py
@@ -15,12 +15,31 @@ def test_list_recipes(client):
     response = client.get('/recipes')
     recipe_list = json.loads(response.data)
     assert('Penne' in recipe_list)
-    assert('Fetuccine' in recipe_list)
     assert('Linguine' in recipe_list)
-    assert('Rigatoni' in recipe_list)
+    assert('Penne Arrabbiata' in recipe_list)
+    assert('Chicken Stir-Fry' in recipe_list)
+    assert('Pancakes' in recipe_list)
 
 def test_unit_conversion(client):
-    pass
+    response = client.get('/recipe?title=Chicken%20Stir-Fry')
+    ingredients_metric = json.loads(response.data)
+
+    response2 = client.get('/recipe?title=Chicken%20Stir-Fry&mass=lb&volume=floz')
+    ingredients_converted = json.loads(response2.data)
+    assert(len(ingredients_metric) == len(ingredients_converted))
+
+    LB_TO_G = 450
+    FLOZ_TO_ML = 340
+
+    # Loop through, check all units were converted and converted quantities are correct
+    for i in range(len(ingredients_metric)):
+        assert(ingredients_converted[i]['unit'] in ['lb', 'floz']) 
+        
+        if ingredients_metric[i]["unit"] == "g":
+            assert(round(ingredients_metric[i]["quantity"] / LB_TO_G, 2) == ingredients_converted[i]['quantity'])
+        else:
+            assert(round(ingredients_metric[i]["quantity"] / FLOZ_TO_ML, 2) == ingredients_converted[i]['quantity'])
+
 
 def test_serving_sizes(client):
     pass
